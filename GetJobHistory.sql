040672b9f6349bbcef697bbdead0209fc7c5c098
SQLFramework <83116685+SQLFrameWork@users.noreply.github.com>
2025-12-04 12:34:49 +0000

    va

---
+++ GetJobHistory.sql
@@ -0,0 +1,140 @@
+﻿DECLARE @JobName nvarchar(1024) = 'syspolicy_purge_history'
+
+DROP TABLE IF EXISTS #Table1
+DROP TABLE IF EXISTS #Table2
+DROP TABLE IF EXISTS #Table3
+
+SET DATEFIRST 1; -- Monday = 1
+
+;WITH Anchor AS
+(
+    SELECT 
+        Today   = CAST(GETDATE() AS date),
+        Base140 = DATEADD(day, -140, CAST(GETDATE() AS date))
+),
+Bounds AS
+(
+    SELECT
+        WeekStart = DATEADD(day, -(DATEPART(weekday, Today) - 1), Today),      -- Monday of current week
+        StartDate = DATEADD(day, -(DATEPART(weekday, Base140) - 1), Base140),  -- Monday before Today-140
+        EndDate   = DATEADD(day, 7 - DATEPART(weekday, Today), Today)          -- Next Sunday after Today
+    FROM Anchor
+),
+Dates AS
+(
+    SELECT StartDate AS DateValue
+    FROM Bounds
+    UNION ALL
+    SELECT DATEADD(day, 1, d.DateValue)
+    FROM Dates d
+    CROSS JOIN Bounds b
+    WHERE d.DateValue < b.EndDate
+)
+SELECT
+    WeekNoRel = DATEDIFF(week, DateValue, (SELECT WeekStart FROM Bounds)) + 1, -- 1=current week, 2=last, …
+    WeekDay   = DATEPART(weekday, DateValue),                                  -- 1=Mon … 7=Sun
+    [Date]    = DateValue
+INTO #Table1
+FROM Dates
+OPTION (MAXRECURSION 2000);
+
+;WITH JobRuns AS
+(
+    SELECT 
+        run_date = CONVERT(date, CONVERT(char(8), h.run_date)), -- yyyymmdd → date
+        run_duration_seconds =
+              ((h.run_duration / 10000) * 3600)          -- hours
+            + (((h.run_duration % 10000) / 100) * 60)     -- minutes
+            +  (h.run_duration % 100)                     -- seconds
+    FROM msdb.dbo.sysjobhistory h
+    INNER JOIN msdb.dbo.sysjobs j
+        ON h.job_id = j.job_id
+    WHERE j.name = @JobName 
+      AND h.step_id = 0 -- only overall job outcome
+)
+SELECT
+    [Date]      = run_date,
+    MaxDuration = MAX(run_duration_seconds)
+INTO #Table2
+FROM JobRuns
+GROUP BY run_date;
+
+;WITH Joined AS
+(
+    SELECT 
+        t1.WeekNoRel,
+        t1.WeekDay,
+        ISNULL(t2.MaxDuration, 0) AS Duration
+    FROM #Table1 t1
+    LEFT JOIN #Table2 t2
+        ON t1.[Date] = t2.[Date]
+),
+Aggregated AS
+(
+    SELECT WeekDay, WeekNoRel, MAX(Duration) AS Duration
+    FROM Joined
+    GROUP BY WeekDay, WeekNoRel
+)
+SELECT WeekDay,
+       ISNULL([1],0)  AS Week1,
+       ISNULL([2],0)  AS Week2,
+       ISNULL([3],0)  AS Week3,
+       ISNULL([4],0)  AS Week4,
+       ISNULL([5],0)  AS Week5,
+       ISNULL([6],0)  AS Week6,
+       ISNULL([7],0)  AS Week7,
+       ISNULL([8],0)  AS Week8,
+       ISNULL([9],0)  AS Week9,
+       ISNULL([10],0) AS Week10,
+       ISNULL([11],0) AS Week11,
+       ISNULL([12],0) AS Week12,
+       ISNULL([13],0) AS Week13,
+       ISNULL([14],0) AS Week14,
+       ISNULL([15],0) AS Week15,
+       ISNULL([16],0) AS Week16,
+       ISNULL([17],0) AS Week17,
+       ISNULL([18],0) AS Week18,
+       ISNULL([19],0) AS Week19,
+       ISNULL([20],0) AS Week20
+       INTO #Table3
+FROM Aggregated
+PIVOT
+(
+    MAX(Duration) FOR WeekNoRel IN (
+        [1],[2],[3],[4],[5],[6],[7],[8],[9],[10],
+        [11],[12],[13],[14],[15],[16],[17],[18],[19],[20]
+    )
+) AS pvt
+ORDER BY WeekDay;
+
+select @JobName JobName
+,CASE WHEN [weekday]= 1 THEN 'Monday'
+WHEN [weekday]= 2 THEN 'Tuesday'
+WHEN [weekday]= 3 THEN 'Wednesday'
+WHEN [weekday]= 4 THEN 'Thursday'
+WHEN [weekday]= 5 THEN 'Friday'
+WHEN [weekday]= 6 THEN 'Saturday'
+WHEN [weekday]= 7 THEN 'Sunday'
+ELSE '' END 'DayOfWeek'
+,Week1	/60 [CurrentWeek (1) ]
+,Week2	/60 Week2
+,Week3	/60 Week3
+,Week4	/60 Week4
+,Week5	/60 Week5
+,Week6	/60 Week6
+,Week7	/60 Week7
+,Week8	/60 Week8
+,Week9	/60 Week9
+,Week10	/60 Week10
+,Week11	/60 Week11
+,Week12	/60 Week12
+,Week13	/60 Week13
+,Week14	/60 Week14
+,Week15	/60 Week15
+,Week16	/60 Week16
+,Week17	/60 Week17
+,Week18	/60 Week18
+,Week19	/60 Week19
+,Week20/60 Week20
+from #Table3
+
---
+++ ReOrg_PartitionedTables.sql
@@ -0,0 +1,314 @@
+﻿USE [DBA_Admin]
+GO
+
+/****** Object:  StoredProcedure [dbo].[ReOrg_PartitionedTables]    Script Date: 02/12/2025 13:27:51 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE   PROCEDURE [dbo].[ReOrg_PartitionedTables]
+(
+      @DatabaseName SYSNAME
+    , @SchemaName   SYSNAME
+    , @TableName    SYSNAME
+      , @FragLevel int = 5
+      ,@Debug int =0
+
+)
+AS
+BEGIN
+    SET NOCOUNT ON
+
+      /*
+### Procedure: ReOrg_PartitionedTables
+**Author:**       Mark.Rollinson@SQLFramework.com  
+**Create date:**  22/11/2025
+**Notes:**  
+  - Re-organizes and updates stats on table as declared above where fragmentation on that partition is > @FragLevel
+  - This resolves a specific problem with Tiger_Prism_Tenant_FHFT_Warehouse where some tables have multiple partitions written to concurrently during index maintenance
+  - This causes deadlocks on those tables so they have been excluded in the main Ola Maintenance run and replaced by this
+  -  Write maintatance results to Ola H CommandLog
+**Parameters :**  
+    @DatabaseName               Specify which database to use
+    @SchemaName                 Schema
+    @TableName                  Table (checks all indexes in that table)
+    @FragLevel                  Defaults to 5%
+      @Debug =1                           Just prints to screen without applying
+   
+*/
+
+    ---------------------------------------------------------------------
+    -- Validate Database Exists, is PRIMARY if in an AG and ONLINE
+    ---------------------------------------------------------------------
+    IF EXISTS (
+        SELECT 1
+        FROM sys.databases
+        WHERE name = @DatabaseName AND replica_id IS NOT NULL AND state_desc = 'ONLINE'
+    )
+    BEGIN
+        IF sys.fn_hadr_is_primary_replica(@DatabaseName) = 0
+        BEGIN
+            RAISERROR('Database %s Not in Primary Replica for this opertation.', 16, 1, @DatabaseName)
+            RETURN
+        END
+ELSE
+    IF NOT  EXISTS (
+        SELECT 1
+        FROM sys.databases
+        WHERE name = @DatabaseName AND replica_id IS NOT NULL AND state_desc = 'ONLINE'
+    )
+
+        BEGIN
+            RAISERROR('Database %s is unavailable for this opertation.', 16, 1, @DatabaseName)
+            RETURN
+        END
+
+    END
+
+    ---------------------------------------------------------------------
+    -- Build full table name and verify it exists
+    ---------------------------------------------------------------------
+DECLARE @TableExists BIT = 0
+DECLARE @sql NVARCHAR(MAX)
+
+SET @sql = '
+    SELECT @TableExists_OUT = CASE
+        WHEN EXISTS (
+            SELECT 1
+            FROM ' + QUOTENAME(@DatabaseName) + '.sys.objects
+            WHERE object_id = OBJECT_ID('''
+                + QUOTENAME(@DatabaseName) + '.'
+                + QUOTENAME(@SchemaName) + '.'
+                + QUOTENAME(@TableName) + ''')
+        ) THEN 1 ELSE 0 END
+'
+
+EXEC sp_executesql
+    @sql,
+    N'@TableExists_OUT BIT OUTPUT',
+    @TableExists_OUT = @TableExists OUTPUT
+
+IF @TableExists = 0
+BEGIN
+    RAISERROR('Table %s.%s.%s does not exist in database %s.',
+              16, 1, @SchemaName, @TableName, @DatabaseName, @DatabaseName)
+    RETURN
+END
+
+    ---------------------------------------------------------------------
+    -- Temp table dor frag results
+    ---------------------------------------------------------------------
+    DROP TABLE IF EXISTS #frag
+
+    CREATE TABLE #frag
+    (
+          object_id INT
+        , index_id INT
+        , partition_number INT
+        , avg_fragmentation_in_percent FLOAT
+        , page_count BIGINT
+    )
+
+    ---------------------------------------------------------------------
+    -- Dynamic SQL for sys.dm_db_index_physical_stats
+    ---------------------------------------------------------------------
+DECLARE @FullObjectName NVARCHAR(400) =
+        QUOTENAME(@DatabaseName) + '.' + QUOTENAME(@SchemaName) + '.' + QUOTENAME(@TableName)
+
+    SET @sql = '
+        INSERT INTO #frag(object_id, index_id, partition_number, avg_fragmentation_in_percent, page_count)
+        SELECT
+              object_id
+            , index_id
+            , partition_number
+            , avg_fragmentation_in_percent
+            , page_count
+        FROM ' + QUOTENAME(@DatabaseName) + '.sys.dm_db_index_physical_stats(
+              DB_ID(''' + @DatabaseName + '''),
+              OBJECT_ID(''' + @FullObjectName + '''),
+              NULL,
+              NULL,
+              ''LIMITED''
+        )
+    '
+    EXEC(@sql)
+
+    ---------------------------------------------------------------------
+    -- Loop partitions
+    ---------------------------------------------------------------------
+    DECLARE
+          @index_id INT
+        , @partition INT
+        , @frag FLOAT
+        , @index_name SYSNAME
+        , @cmd NVARCHAR(MAX)
+        , @startTime DATETIME
+        , @endTime DATETIME
+        , @error INT
+        , @errmsg NVARCHAR(4000)
+        , @cmdType SYSNAME
+
+    DECLARE cur CURSOR LOCAL FAST_FORWARD FOR
+    SELECT index_id, partition_number, avg_fragmentation_in_percent
+    FROM #frag
+    WHERE page_count > 100
+    ORDER BY index_id, partition_number
+
+    OPEN cur
+    FETCH NEXT FROM cur INTO @index_id, @partition, @frag
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+---------------------------------------------------------------------
+-- Get index name from the TARGET database
+---------------------------------------------------------------------
+DECLARE @sqlGetIndexName NVARCHAR(MAX), @idx_out SYSNAME
+
+SET @sqlGetIndexName = '
+    SELECT @idx_out = i.name
+    FROM ' + QUOTENAME(@DatabaseName) + '.sys.indexes i
+    WHERE i.object_id = OBJECT_ID(''' + @FullObjectName + ''')
+      AND i.index_id = ' + CAST(@index_id AS NVARCHAR(10)) + '
+'
+
+EXEC sp_executesql
+      @sqlGetIndexName,
+      N'@idx_out SYSNAME OUTPUT',
+      @idx_out=@idx_out OUTPUT
+
+SET @index_name = @idx_out
+
+        IF @frag > @FragLevel
+        BEGIN
+            ------------------------------------------------------
+            -- REORGANIZE PARTITION
+            ------------------------------------------------------
+            SET @cmd = N'ALTER INDEX ' + QUOTENAME(@index_name) +
+                       N' ON ' + @FullObjectName +
+                       N' REORGANIZE PARTITION = ' + CAST(@partition AS NVARCHAR(12)) + N''
+
+            SET @cmdType = 'INDEX_REORGANIZE'
+            SET @startTime = GETDATE()
+            BEGIN TRY
+                If @Debug = 1
+                        PRINT @cmd
+                        ELSE
+                        BEGIN
+                        EXEC(@cmd)
+                SET @error = 0
+                SET @errmsg = NULL
+                        END
+            END TRY
+            BEGIN CATCH
+                SET @error = ERROR_NUMBER()
+                SET @errmsg = ERROR_MESSAGE()
+            END CATCH
+            SET @endTime = GETDATE()
+                  
+                  IF ISNULL(@Debug,0) = 1
+            INSERT INTO dbo.CommandLog
+            (
+                DatabaseName, SchemaName, ObjectName, IndexName, PartitionNumber,
+                Command, CommandType, StartTime, EndTime, ErrorNumber, ErrorMessage
+            )
+            VALUES
+            (
+                @DatabaseName, @SchemaName, @TableName, @index_name, @partition,
+                @cmd, @cmdType, @startTime, @endTime, @error, @errmsg
+            )
+                  
+            ------------------------------------------------------
+            -- UPDATE STATISTICS FOR PARTITION
+            ------------------------------------------------------
+DECLARE @IsColumnstore BIT = 0
+DECLARE @sqlCheckColstore NVARCHAR(MAX)
+
+SET @sqlCheckColstore = '
+    SELECT @IsColumnstore_OUT = CASE WHEN EXISTS
+    (
+        SELECT 1
+        FROM ' + QUOTENAME(@DatabaseName) + '.sys.indexes i
+        WHERE i.object_id = OBJECT_ID(''' + @FullObjectName + ''')
+          AND i.index_id = ' + CAST(@index_id AS NVARCHAR(10)) + '
+          AND i.type IN (5,6)
+    ) THEN 1 ELSE 0 END
+'
+
+EXEC sys.sp_executesql
+        @sqlCheckColstore,
+        N'@IsColumnstore_OUT BIT OUTPUT',
+        @IsColumnstore_OUT = @IsColumnstore OUTPUT
+
+----------------------------------------------------------------------
+-- Skip UPDATE STATISTICS for columnstore indexes
+----------------------------------------------------------------------
+
+IF @IsColumnstore = 1
+BEGIN
+    PRINT 'Skipping UPDATE STATISTICS for columnstore index: ' + @index_name
+      IF ISNULL(@Debug,0) = 1
+    INSERT INTO dbo.CommandLog
+    (
+        DatabaseName, SchemaName, ObjectName, IndexName, PartitionNumber,
+        Command, CommandType, StartTime, EndTime, ErrorNumber, ErrorMessage
+    )
+    VALUES
+    (
+        @DatabaseName, @SchemaName, @TableName, @index_name, @partition,
+        'Skipped UPDATE STATISTICS due to columnstore index',
+        'UPDATE_STATISTICS_SKIPPED',
+        GETDATE(), GETDATE(), 0, NULL
+    )
+
+END
+ELSE
+
+BEGIN
+            SET @cmd = N'UPDATE STATISTICS ' + QUOTENAME(@DatabaseName) + N'.' +
+                       QUOTENAME(@SchemaName) + N'.' + QUOTENAME(@TableName) + N' ' +
+                       QUOTENAME(@index_name) + N' WITH RESAMPLE ON PARTITIONS (' +
+                       CAST(@partition AS NVARCHAR(12)) + N')'
+
+            SET @cmdType = 'UPDATE_STATISTICS'
+            SET @startTime = GETDATE()
+            BEGIN TRY
+                  If @Debug = 1
+                  PRINT @cmd
+                  ELSE
+                  BEGIN
+              EXEC(@cmd)
+              SET @error = 0
+              SET @errmsg = NULL
+                  END
+            END TRY
+            BEGIN CATCH
+                SET @error = ERROR_NUMBER()
+                SET @errmsg = ERROR_MESSAGE()
+            END CATCH
+            SET @endTime = GETDATE()
+                  IF ISNULL(@Debug,0) = 1
+            INSERT INTO dbo.CommandLog
+            (
+                DatabaseName, SchemaName, ObjectName, IndexName, PartitionNumber,
+                Command, CommandType, StartTime, EndTime, ErrorNumber, ErrorMessage
+            )
+            VALUES
+            (
+                @DatabaseName, @SchemaName, @TableName, @index_name, @partition,
+                @cmd, @cmdType, @startTime, @endTime, @error, @errmsg
+            )
+                  
+        END
+            END
+        FETCH NEXT FROM cur INTO @index_id, @partition, @frag
+    END
+
+    CLOSE cur
+    DEALLOCATE cur
+
+END
+
+GO
+
